f48cd87803b43981358f60820c6eb902c993ea1d
Manuel Macuacua <manitomacuacua8@gmail.com>
2025-10-06 16:42:32 +0200

    Primeiro commit do backend Djangov3

--- backend/settings.py
+++ backend/settings.py
@@ -1,4 +1,64 @@
+import dj_database_url
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / '.env')
+
+SECRET_KEY = os.getenv('SECRET_KEY', 'default-secret')
+DEBUG = os.getenv('DEBUG', 'True') == 'True'
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
+    'api',
+]
+
+MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # para servir arquivos estáticos
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "https://acma-admin.vercel.app",
+    "https://www.acmaconsultancy.com",
+    "https://acmaconsultancy.com",
+    "http://localhost:3000",
+]
+
+DATABASES = {
+    'default': dj_database_url.parse(
+        os.getenv('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True  # Render exige SSL
+    )
+}
+
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+"""
+v1
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qf6-d7m%2sj^vj3n^03p=2awkt@(vynw+u@spn2negx&kw-wq!'
 DEBUG = True
@@ -37,8 +97,8 @@ REST_FRAMEWORK = {
 
 # Configurações CORS
 CORS_ALLOWED_ORIGINS = [
- #   "http://localhost:3000",  # URL do React
- #   "http://127.0.0.1:3000",
+    "http://localhost:3000",  # URL do React
+    "http://127.0.0.1:3000",
     "https://acmaconsultancy.com",
     "https://www.acmaconsultancy.com",
     "https://acma-admin.vercel.app"
@@ -167,3 +227,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/documentos')
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+"""
